5ead1a2071ecb2458ac6e22ebd4534d4c258bf0d
Antonio <antonioru1997@gmail.com>
2021-09-08 14:30:36 +0200

    FINE Esercizio

--- js/main.js
+++ js/main.js
@@ -4,7 +4,18 @@ const app = new Vue(
     {
         el: "#root",
         data: {
-
+            phCounter: 0,
+            photos: ["img/image1.jpg", "img/image2.jpg", "img/image3.jpg", "img/image4.jpg"]
         },
+        methods: {
+            prevPhoto() {
+                this.phCounter -= 1;
+                if (this.phCounter < 0) this.phCounter = (this.photos.length - 1)
+            },
+            nextPhoto() {
+                this.phCounter += 1;
+                if (this.phCounter == (this.photos.length)) this.phCounter = 0
+            }
+        }
     }
 )
